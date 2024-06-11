65e526db15de92d7f9eae05fdfe4e7a877b7a15f
Bruno Pedrazza <pedrazzabruno@gmail.com>
2024-06-11 02:32:08 -0300

    Change factory contract to produce final salt given a parent and salt

--- forwarder-hardhat/contracts/Forwarder.sol
+++ forwarder-hardhat/contracts/Forwarder.sol
@@ -1,23 +1,100 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity 0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Forwarder {
-    address public destination;
+    // Address to which any funds sent to this contract will be forwarded
+    address public parentAddress;
 
-    function init(address _destination) public {
-        require(_destination != address(0), "Destination should not be empty");
-        require(destination == address(0), "Already initialized");
-        destination = _destination;
+    event ForwarderDeposited(address from, uint256 value, bytes data);
+
+    /**
+     * Initialize the contract, and sets the destination address to that of the creator
+     */
+    function init(address _parentAddress) external onlyUninitialized onlyValidParent(_parentAddress) {
+        parentAddress = _parentAddress;
+        uint256 value = address(this).balance;
+
+        if (value == 0) {
+            return;
+        }
+
+        (bool success, ) = parentAddress.call{ value: value }('');
+        require(success, "Flush failed");
+
+        // NOTE: since we are forwarding on initialization,
+        // we don't have the context of the original sender.
+        // We still emit an event about the forwarding but set
+        // the sender to the forwarder itself
+        emit ForwarderDeposited(address(this), value, msg.data);
     }
 
-    function flushERC20(address tokenContractAddress) public {
-        require(destination != address(0), "Not initialized");
+    /**
+     * Execute a token transfer of the full balance from the forwarder token to the parent address
+     * @param tokenContractAddress the address of the erc20 token contract
+     */
+    function flushTokens(address tokenContractAddress) public onlyInitialized {
         IERC20 tokenContract = ERC20(tokenContractAddress);
         uint256 forwarderBalance = tokenContract.balanceOf(address(this));
+        require(forwarderBalance != 0, "Balance should not be zero");
 
-        require(forwarderBalance != 0, "Forwarder balance should not be 0");
-        tokenContract.transfer(destination, forwarderBalance);
+        tokenContract.transfer(parentAddress, forwarderBalance);
+    }
+
+    /**
+     * Flush the entire balance of the contract to the parent address.
+     */
+    function flush() public {
+        uint256 value = address(this).balance;
+
+        if (value == 0) {
+            return;
+        }
+        if (parentAddress == address(0x0)) {
+            return;
+        }
+
+        (bool success, ) = parentAddress.call{ value: value }('');
+        require(success, "Flush failed");
+        emit ForwarderDeposited(msg.sender, value, msg.data);
+    }
+
+    /**
+     * Modifier that will execute internal code block only if the contract has not been initialized yet
+     */
+    modifier onlyUninitialized {
+        require(parentAddress == address(0x0), "Already initialized");
+        _;
+    }
+
+    /**
+     * Modifier that will execute internal code block only if the contract has not been initialized yet
+     */
+    modifier onlyInitialized {
+        require(parentAddress != address(0x0), "Not initialized");
+        _;
+    }
+
+    /**
+     * Modifier that will execute internal code block only if the parent passed is empty
+     */
+    modifier onlyValidParent(address _parentAddress) {
+        require(_parentAddress != address(0x0), "Parent should not be empty");
+        _;
+    }
+
+    /**
+     * Default function; Gets called when data is sent but does not match any other function
+     */
+    fallback() external payable {
+        flush();
+    }
+
+    /**
+     * Default function; Gets called when Ether is deposited with no data, and forwards it to the parent address
+     */
+    receive() external payable {
+        flush();
     }
 }
--- forwarder-hardhat/contracts/ForwarderFactory.sol
+++ forwarder-hardhat/contracts/ForwarderFactory.sol
@@ -1,35 +1,35 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity 0.8.24;
 
 import "./Forwarder.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
 
 contract ForwarderFactory {
-    event ClonedAddress(address indexed cloned);
+    address public implementationAddress;
+
+    event ForwarderCreated(address clonedAddress, address parentAddress);
+
+    constructor(address _implementationAddress) {
+        implementationAddress = _implementationAddress;
+    }
     
-    function cloneForwarder(address forwarder, uint256 salt) public returns (Forwarder clonedForwarder) {
-        address clonedAddress = _createClone(forwarder, salt);
-        Forwarder parentForwarder = Forwarder(forwarder);
-        clonedForwarder = Forwarder(clonedAddress);
-        clonedForwarder.init(parentForwarder.destination());
-        emit ClonedAddress(clonedAddress);
+    function cloneForwarder(address parentAddress, uint256 salt) external {
+        // include the signers in the salt so any contract deployed to a given address must have the same signers
+        bytes32 finalSalt = _getFinalSalt(parentAddress, salt);
+        address clonedAddress = Clones.cloneDeterministic(implementationAddress, finalSalt);
+
+        Forwarder(payable(clonedAddress)).init(parentAddress);
+        emit ForwarderCreated(clonedAddress, parentAddress);
     }
 
-    function _createClone(address target, uint256 salt) private returns (address result) {
-        return Clones.cloneDeterministic(target, bytes32(salt));
-    }
-
-    function predictCloneAddress(address forwarder_, uint256 salt_)
-        public
-        view
-        returns (address)
+    function predictCloneAddress(address parentAddress, uint256 salt) public view returns (address)
     {
-        address predictedAddress =
-            Clones.predictDeterministicAddress(
-                forwarder_,
-                bytes32(salt_)
-            );
-
+        bytes32 finalSalt = _getFinalSalt(parentAddress, salt);
+        address predictedAddress = Clones.predictDeterministicAddress(implementationAddress, finalSalt);
         return predictedAddress;
+    }
+
+    function _getFinalSalt(address parentAddress, uint256 salt) private pure returns (bytes32) {
+        return keccak256(abi.encodePacked(parentAddress, bytes32(salt)));
     }
 }
