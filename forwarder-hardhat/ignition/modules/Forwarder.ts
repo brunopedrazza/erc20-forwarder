d9afe6babbcf077e4a3d499144cf657e7a3da677
Bruno Pedrazza <pedrazzabruno@gmail.com>
2024-06-11 15:19:59 -0300

    Change ignition module to init the master forwarder contract

--- forwarder-hardhat/ignition/modules/Forwarder.ts
+++ forwarder-hardhat/ignition/modules/Forwarder.ts
@@ -1,9 +1,14 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 
-export default buildModule("Forwarder", (m) => {
+const LockModule = buildModule("Forwarder", (m) => {
     const forwarder = m.contract("Forwarder");
+
+    m.call(forwarder, "init", [m.getParameter("destination")])
+    
     const forwarderFactory = m.contract("ForwarderFactory", [forwarder]);
 
     return { forwarder, forwarderFactory };
 });
+
+export default LockModule;
