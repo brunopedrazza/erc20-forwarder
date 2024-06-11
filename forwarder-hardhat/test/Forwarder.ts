886a145df0ecda4409f6c0339cb0b6e66c8119b7
Bruno Pedrazza <pedrazzabruno@gmail.com>
2024-06-11 02:32:49 -0300

    Implement new test cases

--- forwarder-hardhat/test/Forwarder.ts
+++ forwarder-hardhat/test/Forwarder.ts
@@ -1,58 +1,126 @@
 import { expect } from "chai";
 import hre from "hardhat";
 import { Forwarder } from "../src/types/contracts";
+import { parseEther } from "ethers";
 
 describe("Forwarder", function () {
-  const destination = "0x03917b5178B20Bfa1Bce09312AE4EB140b87869e";
+  const parentAddress = "0x03917b5178B20Bfa1Bce09312AE4EB140b87869e";
 
   let forwarder: Forwarder;
+  let forwarderAddress: string;
+  let owner: any;
+  let ownerAddress: string;
 
   beforeEach(async function() {
     const Forwarder = await hre.ethers.getContractFactory("Forwarder");
     forwarder = await Forwarder.deploy();
+    forwarderAddress = await forwarder.getAddress();
+
+    [owner] = await hre.ethers.getSigners();
+    ownerAddress = await owner.getAddress();
   });
 
   describe("Deployment", function () {
-    it("Should not set the destination", async function () {
-      expect(await forwarder.destination()).to.equal("0x0000000000000000000000000000000000000000");
+    it("Should not set the parent address", async function () {
+      expect(await forwarder.parentAddress()).to.equal("0x0000000000000000000000000000000000000000");
     });
   });
 
   describe("Init", function () {
     it("Should not revert if not initialized", async function () {
-      await expect(forwarder.init(destination)).not.to.be.reverted;
+      await expect(forwarder.init(parentAddress)).not.to.be.reverted;
     });
 
     it("Should revert if already initialized", async function () {
-      await forwarder.init(destination);
+      await forwarder.init(parentAddress);
 
-      await expect(forwarder.init(destination)).to.be.revertedWith(
+      await expect(forwarder.init(parentAddress)).to.be.revertedWith(
         "Already initialized"
       );
     });
 
-    it("Should revert if destination is empty", async function () {
+    it("Should revert if parent address is empty", async function () {
       await expect(forwarder.init("0x0000000000000000000000000000000000000000")).to.be.revertedWith(
-        "Destination should not be empty"
+        "Parent should not be empty"
       );
     });
 
-    it("Should init with the right destination", async function () {
-      await forwarder.init(destination);
+    it("Should init with the right parent address", async function () {
+      await forwarder.init(parentAddress);
 
-      expect(await forwarder.destination()).to.equal(destination);
+      expect(await forwarder.parentAddress()).to.equal(parentAddress);
+    });
+
+    it("Should flush correctly when initialized", async function () {
+      const amount = parseEther("1");
+
+      await owner.sendTransaction({ to: forwarderAddress, value: amount });
+
+      const beforeBalance = await owner.provider.getBalance(parentAddress);
+      await forwarder.init(parentAddress);
+      const afterBalance = await owner.provider.getBalance(parentAddress);
+
+      expect(afterBalance).to.equal(beforeBalance + amount);
     });
   });
 
-  describe("Flush ERC20", function () {
+  describe("Flush", function () {
+    it("Should flush correctly on receive", async function () {
+      const amount = parseEther("1");
+      
+      await forwarder.init(parentAddress);
+
+      const beforeBalance = await owner.provider.getBalance(parentAddress);
+      await owner.sendTransaction({ to: forwarderAddress, value: amount });
+      const afterBalance = await owner.provider.getBalance(parentAddress);
+
+      expect(afterBalance).to.equal(beforeBalance + amount);
+    });
+
+    it("Should flush correctly on fallback", async function () {
+      const amount = parseEther("1");
+      
+      await forwarder.init(parentAddress);
+
+      const beforeBalance = await owner.provider.getBalance(parentAddress);
+      await owner.sendTransaction({ to: forwarderAddress, value: amount, data: "0x1234" });
+      const afterBalance = await owner.provider.getBalance(parentAddress);
+
+      expect(afterBalance).to.equal(beforeBalance + amount);
+    });
+
+    it("Should not flush if balance is zero", async function () {
+      const [owner] = await hre.ethers.getSigners();
+
+      const beforeBalance = await owner.provider.getBalance(parentAddress);
+      await forwarder.init(parentAddress);
+      await owner.sendTransaction({ to: forwarderAddress, value: 0 });
+      const afterBalance = await owner.provider.getBalance(parentAddress);
+
+      expect(beforeBalance).to.equal(afterBalance);
+    });
+
+    it("Should emit ForwarderDeposited event on flush", async function () {
+      const amount = parseEther("1");
+
+      await forwarder.init(parentAddress);
+      await owner.sendTransaction({ to: forwarderAddress, value: amount });
+
+      await expect(owner.sendTransaction({ to: forwarderAddress, value: amount }))
+        .to.emit(forwarder, "ForwarderDeposited")
+        .withArgs(ownerAddress, amount, "0x");
+    });
+  });
+
+  describe("Flush Tokens", function () {
     it("Should revert if is not initialized", async function () {
-      await expect(forwarder.flushERC20("0x0000000000000000000000000000000000000000")).to.be.revertedWith(
+      await expect(forwarder.flushTokens("0x0000000000000000000000000000000000000000")).to.be.revertedWith(
         "Not initialized"
       );
     });
 
-    it("Should revert if balance is zero", async function () {
-      await forwarder.init(destination);
+    it("Should revert if token balance is zero", async function () {
+      await forwarder.init(parentAddress);
 
       const amount = 1;
       const TestToken = await hre.ethers.getContractFactory("TestToken");
@@ -60,15 +128,13 @@ describe("Forwarder", function () {
 
       const testTokenAddress = await testToken.getAddress();
 
-      await expect(forwarder.flushERC20(testTokenAddress)).to.be.revertedWith(
-        "Forwarder balance should not be 0"
+      await expect(forwarder.flushTokens(testTokenAddress)).to.be.revertedWith(
+        "Balance should not be zero"
       );
     });
 
-    it("Should send the correct amount to destination", async function () {
-      await forwarder.init(destination);
-
-      const forwarderAddress = await forwarder.getAddress();
+    it("Should send the correct amount to parent address", async function () {
+      await forwarder.init(parentAddress);
 
       const amount = 1;
       const TestToken = await hre.ethers.getContractFactory("TestToken");
@@ -76,9 +142,9 @@ describe("Forwarder", function () {
 
       const testTokenAddress = await testToken.getAddress();
       await testToken.transfer(forwarderAddress, amount);
-      await forwarder.flushERC20(testTokenAddress);
+      await forwarder.flushTokens(testTokenAddress);
 
-      expect(await testToken.balanceOf(destination)).to.equal(amount);
+      expect(await testToken.balanceOf(parentAddress)).to.equal(amount);
     });
   });
 });
--- forwarder-hardhat/test/ForwarderFactory.ts
+++ forwarder-hardhat/test/ForwarderFactory.ts
@@ -1,70 +1,162 @@
 import { expect } from "chai";
 import hre from "hardhat";
-import { Forwarder, ForwarderFactory } from "../src/types/contracts";
+import { ForwarderFactory } from "../src/types/contracts";
+import { extractClonedForwarderAddress } from "../utils/forwarderEventsUtils";
+import { parseEther } from "ethers";
 
 describe("ForwarderFactory", function () {
-  const destination = "0x03917b5178B20Bfa1Bce09312AE4EB140b87869e";
+  const parentAddress = "0x03917b5178B20Bfa1Bce09312AE4EB140b87869e";
+  const parentAddressMalicious = "0x29021D3658fb7aA11383a09117662248e9054223";
 
+  let implementationAddress: string;
   let forwarderFactory: ForwarderFactory;
-  let masterForwarder: Forwarder;
-  let masterForwarderAddress: string;
 
   beforeEach(async function() {
-    const ForwarderFactory = await hre.ethers.getContractFactory("ForwarderFactory");
-    forwarderFactory = await ForwarderFactory.deploy();
-
     const Forwarder = await hre.ethers.getContractFactory("Forwarder");
-    masterForwarder = await Forwarder.deploy();
-    masterForwarderAddress = await masterForwarder.getAddress();
+    const implementation = await Forwarder.deploy();
+    implementationAddress = await implementation.getAddress();
 
-    await masterForwarder.init(destination);
+    const ForwarderFactory = await hre.ethers.getContractFactory("ForwarderFactory");
+    forwarderFactory = await ForwarderFactory.deploy(implementationAddress);
+  });
+
+  describe("Deployment", function () {
+    it("Should set the correct implementation address", async function () {
+      expect(await forwarderFactory.implementationAddress()).to.equal(implementationAddress);
+    });
   });
 
   describe("Cloning", function () {
     it("Should not reject clone", async function () {
-      await expect(forwarderFactory.cloneForwarder(masterForwarderAddress, 1)).not.to.be.rejected;
+      await expect(forwarderFactory.cloneForwarder(parentAddress, 1)).not.to.be.rejected;
     });
 
     it("Should reject negative salt", async function () {
-      await expect(forwarderFactory.cloneForwarder(masterForwarderAddress, -1)).to.be.rejected;
+      await expect(forwarderFactory.cloneForwarder(parentAddress, -1)).to.be.rejected;
     });
 
     it("Should not be able to clone with same salt more than once", async function () {
       const salt = 1;
-      await forwarderFactory.cloneForwarder(masterForwarderAddress, salt);
-      await expect(forwarderFactory.cloneForwarder(masterForwarderAddress, salt)).to.be.rejected;
+      await forwarderFactory.cloneForwarder(parentAddress, salt);
+      await expect(forwarderFactory.cloneForwarder(parentAddress, salt)).to.be.rejected;
     });
 
-    it("Should match master forwarder destination", async function () {
+    it("Should clone with correct parent address", async function () {
       const salt = 1;
-      await forwarderFactory.cloneForwarder(masterForwarderAddress, salt);
-      const predicted = await forwarderFactory.predictCloneAddress(masterForwarderAddress, salt);
+      const cloneResponse = await forwarderFactory.cloneForwarder(parentAddress, salt);
+
+      const clonedForwarderAddress = await extractClonedForwarderAddress(cloneResponse);
 
       const Forwarder = await hre.ethers.getContractFactory("Forwarder");
-      const clonedForwarder = Forwarder.attach(predicted);
-      expect(await clonedForwarder.destination()).to.equal(await masterForwarder.destination());
+      const clonedForwarder = Forwarder.attach(clonedForwarderAddress);
+
+      expect(await clonedForwarder.parentAddress()).to.equal(parentAddress);
+    });
+
+    it("Should not derive same cloned address for different destinations", async function () {
+      const salt = 1;
+
+      const cloneResponse = await forwarderFactory.cloneForwarder(parentAddress, salt);
+      const clonedForwarderAddress = await extractClonedForwarderAddress(cloneResponse);
+
+      const cloneResponseMalicious = await forwarderFactory.cloneForwarder(parentAddressMalicious, salt);
+      const clonedForwarderAddressMalicious = await extractClonedForwarderAddress(cloneResponseMalicious);
+
+      expect(clonedForwarderAddress).not.to.equal(clonedForwarderAddressMalicious);
+    });
+
+    it("Should emit ForwarderCreated event", async function () {
+      await expect(forwarderFactory.cloneForwarder(parentAddress, 1))
+        .to.emit(forwarderFactory, "ForwarderCreated");
     });
   });
 
   describe("Predict Clone", function () {
-    it("Should emit cloned address event with predicted address", async function () {
+    it("Should predict the same address as emitted event", async function () {
       const salt = 1;
-      const predicted = await forwarderFactory.predictCloneAddress(masterForwarderAddress, salt);
-      await expect(forwarderFactory.cloneForwarder(masterForwarderAddress, salt))
-        .to.emit(forwarderFactory, "ClonedAddress")
-        .withArgs(predicted);
+      const predictedAddress = await forwarderFactory.predictCloneAddress(parentAddress, salt);
+      await expect(forwarderFactory.cloneForwarder(parentAddress, salt))
+        .to.emit(forwarderFactory, "ForwarderCreated")
+        .withArgs(predictedAddress, parentAddress);
     });
 
-    it("Should not emit cloned address event with predicted address", async function () {
-      const predicted = await forwarderFactory.predictCloneAddress(masterForwarderAddress, 1);
+    it("Should not predict the same address as emitted event", async function () {
+      const predicted = await forwarderFactory.predictCloneAddress(parentAddress, 1);
 
-      function isDifferentThanPredicted(address: string): boolean {
-        return address != predicted;
+      function isDifferentThanPredicted(clonedAddress: string): boolean {
+        return clonedAddress != predicted;
       }
 
-      await expect(forwarderFactory.cloneForwarder(masterForwarderAddress, 2))
-        .to.emit(forwarderFactory, "ClonedAddress")
-        .withArgs(isDifferentThanPredicted);
+      await expect(forwarderFactory.cloneForwarder(parentAddress, 2))
+        .to.emit(forwarderFactory, "ForwarderCreated")
+        .withArgs(isDifferentThanPredicted, parentAddress);
+    });
+
+    it("Should not predict same address for different parents", async function () {
+      const predicted = await forwarderFactory.predictCloneAddress(parentAddress, 1);
+      const predicted2 = await forwarderFactory.predictCloneAddress(parentAddressMalicious, 1);
+
+      expect(predicted).not.to.equal(predicted2);
+    });
+
+    it("Should not predict same address for different salts", async function () {
+      const predicted = await forwarderFactory.predictCloneAddress(parentAddress, 1);
+      const predicted2 = await forwarderFactory.predictCloneAddress(parentAddress, 2);
+
+      expect(predicted).not.to.equal(predicted2);
+    });
+
+    it("Should not predict same address for different salts and parents", async function () {
+      const predicted = await forwarderFactory.predictCloneAddress(parentAddress, 1);
+      const predicted2 = await forwarderFactory.predictCloneAddress(parentAddressMalicious, 2);
+
+      expect(predicted).not.to.equal(predicted2);
+    });
+  });
+
+  describe("Real-world Scenarios", function () {
+    it("Should predict address, receive tokens on uninitialized clone, initialize and flush token to parent address", async function () {
+      const salt = 99;
+      const amount = BigInt(1);
+      
+      const TestToken = await hre.ethers.getContractFactory("TestToken");
+      const testToken = await TestToken.deploy(amount, "Test", "TEST");
+      const testTokenAddress = await testToken.getAddress();
+
+      const beforeBalance = await testToken.balanceOf(parentAddress);
+
+      const predictedAddress = await forwarderFactory.predictCloneAddress(parentAddress, salt);
+      await testToken.transfer(predictedAddress, amount);
+
+      await forwarderFactory.cloneForwarder(parentAddress, salt);
+
+      const Forwarder = await hre.ethers.getContractFactory("Forwarder");
+      const clonedForwarder = Forwarder.attach(predictedAddress);
+
+      await clonedForwarder.flushTokens(testTokenAddress);
+
+      const afterBalance = await testToken.balanceOf(parentAddress);
+
+      expect(afterBalance).to.equal(beforeBalance + amount);
+    });
+
+    it("Should predict address, receive Ether on uninitialized clone and flush them on initialize", async function () {
+      const salt = 99;
+      const amount = parseEther("1");
+
+      const [owner] = await hre.ethers.getSigners();
+
+      const beforeBalance = await owner.provider.getBalance(parentAddress);
+
+      const predictedAddress = await forwarderFactory.predictCloneAddress(parentAddress, salt);
+
+      await owner.sendTransaction({ to: predictedAddress, value: amount });
+
+      await forwarderFactory.cloneForwarder(parentAddress, salt);
+
+      const afterBalance = await owner.provider.getBalance(parentAddress);
+
+      expect(afterBalance).to.equal(beforeBalance + amount);
     });
   });
 });
